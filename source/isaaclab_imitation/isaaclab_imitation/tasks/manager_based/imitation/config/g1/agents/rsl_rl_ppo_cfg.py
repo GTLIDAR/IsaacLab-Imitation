02c1aac296c016b2155ed4cd33fc92cada63e0cd
Feiyang Wu <feiyangwu@gatech.edu>
2026-02-24 10:16:00 -0500

    Feat: incorporate beyondmimic style imtiation learning rl env.

--- source/isaaclab_imitation/isaaclab_imitation/tasks/manager_based/imitation/config/g1/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_imitation/isaaclab_imitation/tasks/manager_based/imitation/config/g1/agents/rsl_rl_ppo_cfg.py
@@ -10,21 +10,21 @@ from isaaclab_rl.rsl_rl import (
 @configclass
 class G1ImitationPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 1500
-    save_interval = 50
+    max_iterations = 30000
+    save_interval = 500
     experiment_name = "g1_imitation"
     empirical_normalization = False
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
-        actor_hidden_dims=[256, 128, 128],
-        critic_hidden_dims=[256, 128, 128],
+        actor_hidden_dims=[512, 256, 128],
+        critic_hidden_dims=[512, 256, 128],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.008,
+        entropy_coef=0.005,
         num_learning_epochs=5,
         num_mini_batches=4,
         learning_rate=1.0e-3,
